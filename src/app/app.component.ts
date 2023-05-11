766124933ee9e018dc89320620d58ac2d3192b52
sc4n1a471 <martin.terhes@icloud.com>
2023-05-12 00:03:27 +0200

    feat: added auto update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatSidenav} from "@angular/material/sidenav";
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'pwa-project';
     openedPage = ''
+
+    constructor(private update: SwUpdate) {
+    }
+
+    ngOnInit() {
+        this.checkUpdate()
+    }
 
     toggleSidenav(sidenav: MatSidenav) {
         sidenav.toggle()
@@ -18,5 +26,14 @@ export class AppComponent {
         if (event === true) {
             sidenav.close()
         }
+    }
+
+    checkUpdate() {
+        this.update.checkForUpdate().then((canUpdate) => {
+            if (canUpdate) {
+                alert('There is a new version available!');
+                window.location.reload();
+            }
+        });
     }
 }
