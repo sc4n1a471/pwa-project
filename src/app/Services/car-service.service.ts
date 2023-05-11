07fd8bb6bc299e71b7d29bea42d036121af414af
sc4n1a471 <martin.terhes@icloud.com>
2023-05-11 20:35:56 +0200

    feat: added car data status

--- src/app/Services/car-service.service.ts
+++ src/app/Services/car-service.service.ts
@@ -14,16 +14,28 @@ export class CarServiceService {
     private _cars$ = new BehaviorSubject<Car[]>([])
     readonly cars$ = this._cars$.asObservable()
 
+    private _carsLoaded = false;
+
     constructor(private http: HttpClient) { }
 
     async getCars() {
-        this.http.get<CarsResponse>("http://cars.anton.local/cars")
-            .subscribe((response: CarsResponse) => {
-                if (response.success) {
-                    this._cars$.next(response.cars)
-                } else {
-                    console.log(response.success, response.message)
-                }
-            })
+        if (!this._carsLoaded) {
+            this.http.get<CarsResponse>("http://cars.anton.local/cars")
+                .subscribe((response: CarsResponse) => {
+                    if (response.success) {
+                        console.log(`${response.cars.length} cars are downloaded`)
+                        this._cars$.next(response.cars)
+                        this._carsLoaded = true;
+                    } else {
+                        console.log(response.success, response.message)
+                    }
+                })
+        } else {
+            console.log("Cars are already loaded!")
+        }
+    }
+
+    get carsLoaded(): boolean {
+        return this._carsLoaded;
     }
 }
